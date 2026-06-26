0b7ca1757553d00bfb5beaffdf3405a4a6d3d008
tak <kartikthakkannavar2004@gmail.com>
2026-06-26 23:47:14 +0530

    first commit

---
+++ k1/src/test/java/kartik1/k1/AppTest.java
@@ -0,0 +1,19 @@
+package kartik1.k1;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
